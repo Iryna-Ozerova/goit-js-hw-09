c7ce2be54499a56a4e00743f23b8dd4738763017
Irina Ozerova <marta.ilyashenko@gmail.com>
2025-02-23 02:41:23 +0200

    add form.css

---
+++ src/js/2-form.js
@@ -0,0 +1,13 @@
+const form = document.querySelector(".feedback-form");
+const localStorageKey = "feedback-form-state";
+
+const formData = {
+  email: "",
+  message: ""
+};
+
+
+
+form.addEventListener("input", (event) => {
+    localStorage
+}) 
