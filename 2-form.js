c81e644c1beec7de79b616cc1a233050a7d730ed
Iryna-Ozerova <marta.ilyashenko@gmail.com>
2025-02-23 00:41:42 +0000

    Deploying to gh-pages from @ Iryna-Ozerova/goit-js-hw-09@c7ce2be54499a56a4e00743f23b8dd4738763017 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                     */const e=document.querySelector(".feedback-form");e.addEventListener("input",t=>{});
+//# sourceMappingURL=2-form.js.map
